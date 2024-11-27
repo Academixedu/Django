dbc8fc2e18659006b1eb09673490a78d463f9433
babitha0305 <marpally.babithagoud@gmail.com>
2024-11-27 11:23:04 +0530

    completed

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -10,3 +10,24 @@ def sample1(request):
 def sample2(request):
     return JsonResponse({"message":"Hello JSON"}) 
 
+def sample_text_view(request):
+    content = """
+    Welcome to our Django app!
+
+    we are Learning Django
+
+    Thank you for visiting!
+    """
+    return HttpResponse(content, content_type="text/plain")
+
+def new(request):
+    content = """
+    Welcome to our Django app!
+
+    we are Learning Django
+
+    Thank you for visiting!
+    
+    """
+    return JsonResponse({"message": content})
+
